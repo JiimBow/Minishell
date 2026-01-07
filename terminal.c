3c7dd6e6fbdc1791956ce84115428ad9fe5da543
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-01-07 15:49:59 +0100

    test prompt

--- terminal.c
+++ terminal.c
@@ -3,22 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   terminal.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgarnier <mgarnier@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 11:52:55 by jodone            #+#    #+#             */
-/*   Updated: 2026/01/07 14:34:19 by mgarnier         ###   ########.fr       */
+/*   Updated: 2026/01/07 15:45:29 by jodone           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+
+void	handle_sigint(int sig)
+{
+	if (sig == SIGINT)
+	{
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		printf("\n");
+		rl_redisplay();
+	}
+	else if (sig == SIGQUIT)
+	{
+		printf("");
+		rl_redisplay();
+	}
+}
 
 int	main(void)
 {
 	char	*line;
 
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, handle_sigint);
 	while (1)
 	{
 		line = readline("minishell> ");
@@ -27,5 +46,8 @@ int	main(void)
 			add_history(line);
 			free(line);
 		}
+		else if(!line)
+			break ;
 	}
+	return (0);
 }
