537676b05ec2e28a62c43cb4c06db7ea93bead7f
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-01-07 17:21:06 +0100

    add file.h

---
+++ includes/minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/07 16:50:13 by jodone            #+#    #+#             */
+/*   Updated: 2026/01/07 16:57:34 by jodone           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef struct s_signal
+{
+	int	SIG;
+}	t_signal;
+
+#endif
