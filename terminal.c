390f855b0f48f1ba31a304ffae2e9ae678b675c5
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-01-07 14:12:20 +0100

    first commit

---
+++ terminal.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   terminal.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/07 11:52:55 by jodone            #+#    #+#             */
+/*   Updated: 2026/01/07 13:55:33 by jodone           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main()
+{
+	char	*line;
+
+	while (1)
+	{
+		line = readline("minishell >");
+		if (line)
+		{
+			add_history(line);
+			free(line);
+		}
+	}
+}
