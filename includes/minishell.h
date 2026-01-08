b45467f8e741b71bba0ef5132ca3660faad72574
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-01-08 10:48:44 +0100

    .h modifié pour libft

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,22 +6,30 @@
 /*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 16:50:13 by jodone            #+#    #+#             */
-/*   Updated: 2026/01/07 16:57:34 by jodone           ###   ########.fr       */
+/*   Updated: 2026/01/08 10:48:15 by jodone           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "libft.h"
+# include "get_next_line.h"
+# include "ft_printf.h"
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 # include <signal.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <sys/types.h>
 
-typedef struct s_signal
+typedef struct s_cmd
 {
-	int	SIG;
-}	t_signal;
+	char	*cmd;
+	char	**envp;
+}	t_cmd;
+
+void	process(char *cmd, char **envp);
 
 #endif
