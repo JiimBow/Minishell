f41641708c323df3eb9b60d369646565f76af5c4
mgarnier <mgarnier@2E1.42angouleme.fr>
2026-01-08 16:29:08 +0100

    link entre split_line et cmd_process, pas normer

--- sources/main.c
+++ sources/main.c
@@ -6,11 +6,27 @@
 /*   By: mgarnier <mgarnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 11:52:55 by jodone            #+#    #+#             */
-/*   Updated: 2026/01/08 14:30:09 by mgarnier         ###   ########.fr       */
+/*   Updated: 2026/01/08 16:00:06 by mgarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	free_double_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	if (!tab || !*tab)
+		return ;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	tab = NULL;
+}
 
 void	handle_sigint(int sig)
 {
@@ -26,6 +42,7 @@ void	handle_sigint(int sig)
 int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
+	char	**args;
 
 	(void)argc;
 	(void)argv;
@@ -41,13 +58,11 @@ int	main(int argc, char **argv, char **envp)
 			rl_clear_history();
 			exit(0);
 		}
-		parse_line(line);
-		if (line && *line)
-		{
-			process(line, envp);
-			add_history(line);
-			free(line);
-		}
+		args = parse_line(line);
+		process(args, envp);
+		add_history(line);
+		free(line);
+		free_double_tab(args);
 	}
 	return (0);
 }
