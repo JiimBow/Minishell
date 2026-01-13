f0689bd881b8e78246e8e3e90a54f5f3812ea9ab
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-01-13 18:07:50 +0100

    exit fonctionnel et renvoie le bon signal d'erreur

---
+++ sources/built-in/ft_exit.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exit.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 14:53:51 by jodone            #+#    #+#             */
+/*   Updated: 2026/01/13 18:06:14 by jodone           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+void	free_and_exit(t_line *line, int sig_exit)
+{
+	free(line->line);
+	if (sig_exit || !line->args[1])
+	{
+		free_double_tab(line->args);
+		free(line);
+		exit(sig_exit);
+	}
+	free_double_tab(line->args);
+	free(line);
+	exit(2);
+}
+
+int	arg_isnum(char *args)
+{
+	int	i;
+
+	i = 0;
+	while (args && args[i])
+	{
+		if (!ft_isdigit(args[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	free_before_exit(t_line *line, t_env *env, t_arg *data, int sig_return)
+{
+	printf("exit\n");
+	if (line->args && line->args[1] && line->args[2])
+	{
+		printf("minishell: exit: too many arguments\n");
+		return ;
+	}
+	rl_clear_history();
+	free_double_tab(env->env);
+	if (env)
+		free(env);
+	if (line->args && !arg_isnum(line->args[1]))
+		printf("minishell: exit: %s: numeric argument required\n",
+			line->args[1]);
+	else if (line->args && !sig_return)
+		sig_return = ft_atoi(line->args[1]);
+	free_struct(data);
+	if (line->line)
+		free_and_exit(line, sig_return);
+	free_double_tab(line->args);
+	free(line);
+	exit(sig_return);
+}
