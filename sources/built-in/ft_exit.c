0804181dd0ae0e901aac3e28d2fdea4f9b4dcb68
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-01-28 15:15:36 +0100

    fix line->prev_sig remplacant line->sig pour exit

--- sources/built-in/ft_exit.c
+++ sources/built-in/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 14:53:51 by jodone            #+#    #+#             */
-/*   Updated: 2026/01/23 12:39:15 by jodone           ###   ########.fr       */
+/*   Updated: 2026/01/28 15:14:37 by jodone           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static void	write_exit(t_line *line, int code)
 	write(2, line->args[1], ft_strlen(line->args[1]));
 	write(2, ": numeric argument required\n", 29);
 	if (code == 1)
-		line->sig = 2;
+		line->prev_sig = 2;
 }
 
 static void	free_and_exit(t_line *line)
@@ -26,8 +26,8 @@ static void	free_and_exit(t_line *line)
 	int	exit_code;
 
 	exit_code = 2;
-	if (line->sig != 0 || (line->args && !line->args[1]))
-		exit_code = line->sig;
+	if (line->prev_sig != 0 || (line->args && !line->args[1]))
+		exit_code = line->prev_sig;
 	free_line_struct(line, 1);
 	exit(exit_code);
 }
@@ -63,15 +63,15 @@ int	free_before_exit(t_line *line, t_var *lst_var)
 	ft_lstclear_var(&lst_var, free);
 	if (line->args && !arg_isnum(line->args[1]))
 		write_exit(line, 0);
-	else if (line->args && !line->sig)
+	else if (line->args && !line->prev_sig)
 	{
-		line->sig = ft_atoll(line->args[1], &overflow);
+		line->prev_sig = ft_atoll(line->args[1], &overflow);
 		if (overflow == -1)
 			write_exit(line, 1);
 	}
 	if (line->line)
 		free_and_exit(line);
-	overflow = line->sig;
+	overflow = line->prev_sig;
 	free_line_struct(line, 1);
 	exit(overflow);
 }
