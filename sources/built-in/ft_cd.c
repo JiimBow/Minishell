fab1d6bfa5f7a3b7147d45a09859d4a61d431df8
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-02-10 17:25:36 +0100

    fix cd ~ et cd sans arguments leak

--- sources/built-in/ft_cd.c
+++ sources/built-in/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 16:44:39 by jodone            #+#    #+#             */
-/*   Updated: 2026/02/10 17:09:13 by jodone           ###   ########.fr       */
+/*   Updated: 2026/02/10 17:24:13 by jodone           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,11 +64,15 @@ static void	update_path(t_var *lst_var, int is_old)
 
 static char	*get_path(t_line *line, t_var *lst_var)
 {
-	char *path;
+	char	*path;
 
 	path = NULL;
-	if (!line->args[1] || ft_strncmp(line->args[1], "~", 2) == 0)
+	if (!line->args[1] || ft_strncmp(line->args[1], "~", 1) == 0)
+	{
 		path = get_env_path(lst_var, "HOME");
+		if (line->args[1] && line->args[1][1])
+			path = gnl_strjoin(path, line->args[1] + 1);
+	}
 	else if (ft_strncmp(line->args[1], ".", 2) == 0)
 		path = get_env_path(lst_var, "OLDPWD");
 	else
