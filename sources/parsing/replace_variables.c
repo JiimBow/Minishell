34d04ea55691074b11e86167ab0ec71870ae26d8
mgarnier <mgarnier@2E1.42angouleme.fr>
2026-02-10 11:51:05 +0100

    travail en cours

--- sources/parsing/replace_variables.c
+++ sources/parsing/replace_variables.c
@@ -6,62 +6,71 @@
 /*   By: mgarnier <mgarnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 16:46:45 by mgarnier          #+#    #+#             */
-/*   Updated: 2026/02/10 10:48:56 by mgarnier         ###   ########.fr       */
+/*   Updated: 2026/02/10 11:31:59 by mgarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	**create_copy(t_line *line, t_var *lst_var)
+{
+	char	**copy;
+	int	i;
+
+	i = 0;
+	while (line->args[i])
+		i++;
+	copy = (char **)malloc(sizeof(char *) * (i + 1));
+	if (!copy)
+		error_memory_failed(line, lst_var);
+	i = 0;
+	while (line->args[i])
+	{
+		copy[i] = ft_strdup(line->args[i]);
+		free(line->args[i]);
+		line->args[i] = NULL;
+		i++;
+	}
+	copy[i] = NULL;
+	return (copy);
+}
+
 void	replace_variables(t_line *line, t_var *lst_var)
 {
 	char	**copy;
-	char	**tab;
+	char	**word;
 	char	**var;
 	int		i;
 	int		j;
 	int		x;
 	int		y;
 
-	j = 0;
-	while (line->args[j])
-		j++;
-	copy = (char **)malloc(sizeof(char *) * (j + 1));
-	if (!copy)
-		error_memory_failed(line, lst_var);
-	j = 0;
-	while (line->args[j])
-	{
-		copy[j] = ft_strdup(line->args[j]);
-		free(line->args[j]);
-		line->args[j] = NULL;
-		j++;
-	}
-	copy[j] = NULL;
+	copy = create_copy(line, lst_var);
 	j = 0;
 	i = 0;
 	while (copy[j])
 	{
-		tab = split_words(line, lst_var, copy[j]);
+		word = split_words(line, lst_var, copy[j]);
 		x = 0;
-		while (tab[x])
+		while (word[x])
 		{
-			if (is_quote(tab[x][0]))
+			if (is_quote(word[x][0]))
 			{
-				tab[x] = substr_var_unquote(line, lst_var, tab[x]);
-				line->args[i] = gnl_strjoin(line->args[i], tab[x]);
+				word[x] = substr_var_unquote(line, lst_var, word[x]);
+				line->args[i] = gnl_strjoin(line->args[i], word[x]);
 			}
 			else
 			{
-				if (tab[x][0] == '$' && tab[x][1] == '\0' && tab[x + 1] != NULL)
+				if (ft_strnstr(word[x], "$", 2) && word[x + 1] != NULL)
 				{
 					x++;
 					continue ;
 				}
-				tab[x] = substr_var_unquote(line, lst_var, tab[x]);
-				if (ft_strchr(tab[x], '=') > ft_strchr(tab[x], ' '))
-					var = split_spaces(line, lst_var, tab[x]);
+				word[x] = substr_var_unquote(line, lst_var, word[x]);
+				if (ft_strchr(word[x], '=') > ft_strchr(word[x], ' '))
+					var = split_spaces(line, lst_var, word[x]);
 				else
-					var = ft_split(tab[x], '\0');
+					var = ft_split(word[x], '\0');
 				y = 0;
 				while (var && var[y])
 				{
@@ -75,7 +84,7 @@ void	replace_variables(t_line *line, t_var *lst_var)
 			}
 			x++;
 		}
-		free_double_tab(tab);
+		free_double_tab(word);
 		j++;
 		i++;
 	}
