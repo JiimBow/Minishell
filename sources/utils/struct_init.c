6068585343b491ee5d3208979f9677d8b3776e43
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-02-11 11:15:30 +0100

    modif initialisation

--- sources/utils/struct_init.c
+++ sources/utils/struct_init.c
@@ -6,7 +6,7 @@
 /*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 11:09:31 by jodone            #+#    #+#             */
-/*   Updated: 2026/02/10 17:29:07 by jodone           ###   ########.fr       */
+/*   Updated: 2026/02/11 11:14:28 by jodone           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,6 @@ t_line	*creation_line(t_var *lst_var)
 	line = (t_line *)malloc(sizeof(t_line));
 	if (!line)
 		error_memory_failed(NULL, lst_var);
-	line->env = NULL;
-	line->new = NULL;
-	line->block = NULL;
 	line->home = get_env_path(lst_var, "HOME");
 	line->sig = 0;
 	line->prev_sig = 0;
